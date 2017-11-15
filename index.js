1124aee7de2568327bf66a40ffec0551c3d76ff1
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-15 13:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(array) {
-  var drivers = array.map(function(x){
+  return array.map(function (x){
     return x.toLowerCase();
   })
 }
